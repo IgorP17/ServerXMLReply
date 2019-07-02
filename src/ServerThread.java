522de28465c34436e52ec7c88d998dadbceb7764
Prokhorov <prokhorov@bpcbt.com>
2019-07-02 11:12:08 +0300

    go

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.stream.Stream;
 
 public class ServerThread extends Thread{
     private Socket socket;
@@ -17,15 +18,27 @@ public class ServerThread extends Thread{
             PrintWriter writer = new PrintWriter(output, true);
 
 
-            String text;
+            /*String text;
 
             do {
                 text = reader.readLine();
                 String reverseText = new StringBuilder(text).reverse().toString();
                 writer.println("Server: " + reverseText);
 
-            } while (!text.equals("bye"));
-
+            } while (!text.equals("bye"));*/
+            String text;
+            StringBuilder inString = new StringBuilder();
+            System.out.println("Reading");
+            while (true){
+                
+                text = reader.readLine();
+                if (text == null) break;
+                inString.append(text);
+                System.out.println("GOT:" + text);
+            }
+            System.out.println("Done!");
+            writer.print(inString.toString());
+            System.out.println(inString.toString());
             socket.close();
         } catch (IOException ex) {
             System.out.println("Server exception: " + ex.getMessage());
